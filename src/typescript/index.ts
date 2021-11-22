b167a86f7300d911fe0790b8dcbc21c66eb0f4e4
Chucky Master <user.email>
2021-11-22 22:44:59 +0100

    function display

--- src/typescript/index.ts
+++ src/typescript/index.ts
@@ -1,12 +1,5 @@
 import "..//scss/style.scss";
 
-// let test: number = 10;
-// let fuckoff: string = "For god sake";
-
-// let numero: number = 458;
-
-// console.log("putain");
-
 //SELECTORS
 
 //WEB COMPONENT
@@ -20,19 +13,53 @@ type jeuxVideo = {
   anneeSortie: number;
   multijoueur: boolean;
   enligne: boolean;
-  image: string;
+  image: URL;
 };
 
-//ARRAY
+//ARRAY // objet
 
-// let listJeux: jeuxVideo = [
+let listJeux: jeuxVideo[] = [
+  {
+    nom: "Mortal kombat",
+    genre: "combat",
+    editeur: "je sais pas la honte",
+    pegi: 12,
+    anneeSortie: 1895,
+    multijoueur: true,
+    enligne: true,
+    image: new URL(
+      "https://upload.wikimedia.org/wikipedia/fr/thumb/e/e4/Mortal_Kombat_II_Logo.svg/1200px-Mortal_Kombat_II_Logo.svg.png"
+    ),
+  },
+];
 
-// ]
+let game1: jeuxVideo = {
+  nom: "Mortal kombat",
+  genre: "combat",
+  editeur: "je sais pas la honte",
+  pegi: 12,
+  anneeSortie: 1895,
+  multijoueur: true,
+  enligne: true,
+  image: new URL(
+    "https://upload.wikimedia.org/wikipedia/fr/thumb/e/e4/Mortal_Kombat_II_Logo.svg/1200px-Mortal_Kombat_II_Logo.svg.png"
+  ),
+};
 
 //FUNCTION
-// function catchValue(objInput: object) {
-// let obj: jeuxVideo = {
 
-// }
+function display(list: jeuxVideo) {
+  let affichJeux: HTMLElement = document.querySelector("#listJeux");
+  let ul: HTMLElement = document.createElement("ul");
+  affichJeux.appendChild(ul);
+  for (const property in list) {
+    if (property != "image") {
+      let li: HTMLElement = document.createElement("li");
+      ul.appendChild(li);
 
-// }
+      li.innerHTML += `${property} : ${list[property]}`;
+    }
+  }
+}
+
+display(game1);
